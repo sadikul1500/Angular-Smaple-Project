3fcaf2dcff8d990451b672154b93187e48632ca6
sadikul1500 <bsse1003@iit.du.ac.bd>
2023-05-23 16:27:31 +0600

    4 files modified, 10 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,10 +9,22 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartSetvice: CartService
+  ) {}
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productId = Number(routeParams.get('productId'));
     this.product = products.find((p) => p.id === productId);
+  }
+
+  addToCart() {
+    if (this.product) {
+      this.cartSetvice.addToCart(this.product);
+      window.alert('The product has been added to the cart');
+    } else {
+      window.alert('Invalid operation');
+    }
   }
 }
